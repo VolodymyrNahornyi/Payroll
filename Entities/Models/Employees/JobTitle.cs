9458cb17009fdcf637f41285c119863a2989dd9e
VolodymyrNahornyi <52907011+VolodymyrNahornyi@users.noreply.github.com>
2021-05-29 11:21:06 +0300

    Create JobTitle model

---
+++ Entities/Models/Employees/JobTitle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities.Models.Employees
+{
+    public class JobTitle
+    {
+        [Column("JobTitleId")]
+        public Guid Id { get; set; }
+        
+        [Required(ErrorMessage = "Employee job title is a required field.")]
+        [MaxLength(30, ErrorMessage = "Maximum length for the Job Title is 30 characters.")]
+        public string Title { get; set; }
+    }
+}
