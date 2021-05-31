5a8af584a5308b8b0303a46d215e6ae7fc79b469
VolodymyrNahornyi <52907011+VolodymyrNahornyi@users.noreply.github.com>
2021-05-31 10:57:28 +0300

    Create City model

---
+++ Entities/Models/Employees/City.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities.Models.Employees
+{
+    public class City
+    {
+        [Column("CityId")]
+        public Guid Id { get; set; }
+        
+        [Required(ErrorMessage = "Employee city is a required field.")]
+        [MaxLength(30, ErrorMessage = "Maximum length for the city is 30 characters.")]
+        public string CityName { get; set; }
+        
+        [ForeignKey(nameof(Country))]
+        public Guid CountryId { get; set; }
+        public Country Country { get; set; }
+    }
+}
