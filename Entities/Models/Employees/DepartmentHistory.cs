c291317b2c6bd71fbb3b79dc1929f094b2094961
VolodymyrNahornyi <52907011+VolodymyrNahornyi@users.noreply.github.com>
2021-05-31 10:50:33 +0300

    Create Department History model

---
+++ Entities/Models/Employees/DepartmentHistory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities.Models.Employees
+{
+    public class DepartmentHistory
+    {
+        [Column("DepartmentHistoryId")]
+        public Guid Id { get; set; }
+        
+        [ForeignKey(nameof(Department))]
+        public Guid DepartmentId { get; set; }
+        public Department Department { get; set; }
+        
+        [ForeignKey(nameof(Employee))]
+        public Guid EmployeeId { get; set; }
+        public Employee Employee { get; set; }
+        
+        [Required(ErrorMessage = "Employee department start date is a required field.")]
+        public DateTime StartDate { get; set; }
+        
+        [Required(ErrorMessage = "Employee department end date is a required field.")]
+        public DateTime EndDate { get; set; }
+    }
+}
