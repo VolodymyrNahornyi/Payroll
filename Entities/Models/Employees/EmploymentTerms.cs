47ab66e83a38c729a8ff0c9bfbca2489528482c4
VolodymyrNahornyi <52907011+VolodymyrNahornyi@users.noreply.github.com>
2021-05-31 11:22:10 +0300

    Create Employment Terms model

---
+++ Entities/Models/Employees/EmploymentTerms.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities.Models.Employees
+{
+    public class EmploymentTerms
+    {
+        [Column("EmploymentTermsId")]
+        public Guid Id { get; set; }
+        
+        [ForeignKey(nameof(Employee))]
+        public Guid EmployeeId { get; set; }
+        public Employee Employee { get; set; }
+        
+        [Required(ErrorMessage = "Employee agreed salary is a required field.")]
+        public decimal AgreedSalary { get; set; }
+        
+        [Required(ErrorMessage = "Employee salary start date is a required field.")]
+        public DateTime SalaryStartDate { get; set; }
+        
+        [Required(ErrorMessage = "Employee salary end date is a required field.")]
+        public DateTime SalaryEndDate { get; set; }
+    }
+}
