c11e905aee2801b47530a4bf1a2096787134e8b7
VolodymyrNahornyi <52907011+VolodymyrNahornyi@users.noreply.github.com>
2021-05-31 10:53:38 +0300

    Create Gender model

---
+++ Entities/Models/Employees/Gender.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities.Models.Employees
+{
+    public class Gender
+    {
+        [Column("GenderId")]
+        public Guid Id { get; set; }
+        
+        [Required(ErrorMessage = "Employee gender is a required field.")]
+        public string GenderName { get; set; }
+    }
+}
