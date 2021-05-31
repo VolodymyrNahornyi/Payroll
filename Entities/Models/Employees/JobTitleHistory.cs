3310f9e438279fcfcca9c69c1d4f7ca8aede0f72
VolodymyrNahornyi <52907011+VolodymyrNahornyi@users.noreply.github.com>
2021-05-31 10:11:33 +0300

    Create Job Title History model

---
+++ Entities/Models/Employees/JobTitleHistory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities.Models.Employees
+{
+    public class JobTitleHistory
+    {
+        [Column("JobTitleHistoryId")]
+        public Guid Id { get; set; }
+        
+        [ForeignKey(nameof(JobTitle))]
+        public Guid JobTitleId { get; set; }
+        public JobTitle JobTitle { get; set; }
+        
+        [ForeignKey(nameof(Employee))]
+        public Guid EmployeeId { get; set; }
+        public Employee Employee { get; set; }
+        
+        [Required(ErrorMessage = "Employee employment start date is a required field.")]
+        public DateTime StartDate { get; set; }
+        
+        [Required(ErrorMessage = "Employee employment end date is a required field.")]
+        public DateTime EndDate { get; set; }
+    }
+}
