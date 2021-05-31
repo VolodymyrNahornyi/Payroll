9bb3a55bbfe19664b78a6760cd68a0f1905701f9
VolodymyrNahornyi <52907011+VolodymyrNahornyi@users.noreply.github.com>
2021-05-31 11:59:52 +0300

    Create Salary Payment model

---
+++ Entities/Models/Salaries/SalaryPayment.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Entities.Models.Employees;
+
+namespace Entities.Models.Salaries
+{
+    public class SalaryPayment
+    {
+        [Column("SalaryPaymentId")] 
+        public Guid Id { get; set; }
+
+        [ForeignKey(nameof(Employee))] 
+        public Guid EmployeeId { get; set; }
+        public Employee Employee { get; set; }
+
+        [Required(ErrorMessage = "Employee gross salary is a required field.")]
+        public string GrossSalary { get; set; }
+
+        [Required(ErrorMessage = "Employee net salary is a required field.")]
+        public string NetSalary { get; set; }
+
+        [Required(ErrorMessage = "Employee salary period is a required field.")]
+        public DateTime SalaryPeriod { get; set; }
+    }
+}
