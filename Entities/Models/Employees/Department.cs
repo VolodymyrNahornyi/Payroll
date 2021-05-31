e4e40445e64ba0767489dee38e99fa260fae81d5
VolodymyrNahornyi <52907011+VolodymyrNahornyi@users.noreply.github.com>
2021-05-31 10:47:48 +0300

    Create Department model

---
+++ Entities/Models/Employees/Department.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities.Models.Employees
+{
+    public class Department
+    {
+        [Column("DepartmentId")]
+        public Guid Id { get; set; }
+        
+        [Required(ErrorMessage = "Department Name is a required field.")]
+        [MaxLength(30, ErrorMessage = "Maximum length for the department name is 30 characters.")]
+        public string DepartmentName { get; set; }
+    }
+}
