b4e64a0fe3b7ec00830ade6f9c892c44aed67b9e
VolodymyrNahornyi <52907011+VolodymyrNahornyi@users.noreply.github.com>
2021-05-31 11:03:26 +0300

    Create Country model

---
+++ Entities/Models/Employees/Country.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities.Models.Employees
+{
+    public class Country
+    {
+        [Column("CountryId")]
+        public Guid Id { get; set; }
+        
+        [Required(ErrorMessage = "Employee country name is a required field.")]
+        [MaxLength(30, ErrorMessage = "Maximum length for the country name is 30 characters.")]
+        public string CountryName { get; set; }
+    }
+}
